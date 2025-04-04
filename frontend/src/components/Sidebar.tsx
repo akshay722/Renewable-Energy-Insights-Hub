060625da76d69fca7dc88886864c95144e705f66
Akshay Varma <akshayvarmamit@gmail.com>
2025-04-03 23:17:56 -0500

    feat(frontend): change pages in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -29,8 +29,8 @@ const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
       ),
     },
     {
-      name: 'Consumption',
-      path: '/consumption',
+      name: 'Energy Tracking',
+      path: '/energy-tracking',
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -43,27 +43,7 @@ const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            d="M13 10V3L4 14h7v7l9-11h-7z"
-          />
-        </svg>
-      ),
-    },
-    {
-      name: 'Generation',
-      path: '/generation',
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
           />
         </svg>
       ),

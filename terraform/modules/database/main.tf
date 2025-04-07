e0d0fdb0f1ffc11b9cc4dc1adffbfbcd6dce8ae6
Akshay Varma <akshayvarmamit@gmail.com>
2025-04-06 22:10:21 -0500

    build: update RDS version

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "database" {
   identifier           = "renewable-energy-db"
   engine               = "mysql"
   engine_version       = "8.0.37"
-  instance_class       = "db.t2.micro" # Free tier eligible
+  instance_class       = "db.t3.micro" # Free tier eligible
   db_name              = var.db_name
   username             = var.db_user
   password             = var.db_password

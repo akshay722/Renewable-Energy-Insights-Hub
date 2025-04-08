21d52b620c11e02072e7c1292fe52190074e30c6
Akshay Varma <akshayvarmamit@gmail.com>
2025-04-07 23:40:39 -0500

    refactor: cleanup terraforms

--- terraform/modules/frontend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket for frontend"
-  type        = string
-}
-
-variable "region" {
-  description = "AWS region to deploy the S3 bucket"
-  type        = string
-} 
